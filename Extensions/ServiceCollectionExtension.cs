8d9fba2e69b97c6bc298d470136de66d547ce15c
martinpakev <marti_pakev@abv.bg>
2024-12-16 20:37:55 +0200

    Database created

--- Extensions/ServiceCollectionExtension.cs
+++ Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,4 @@
-﻿using HouseRentingSystem.Data;
+﻿using HouseRentingSystem.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
         {
             var connectionString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-           services.AddDbContext<ApplicationDbContext>(options =>
+           services.AddDbContext<HouseRentingDbContext>(options =>
                 options.UseSqlServer(connectionString));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
@@ -27,7 +27,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services
                .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-               .AddEntityFrameworkStores<ApplicationDbContext>();
+               .AddEntityFrameworkStores<HouseRentingDbContext>();
 
             return services;
         }
